9f4c09e92c8c916fa6daa598b16f1c3e0a84229d
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-09 14:35:02 -0700

    made a seed file for users for testing purpose

--- models/Post.js
+++ models/Post.js
@@ -1,40 +1,40 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+// const { Model, DataTypes } = require("sequelize");
+// const sequelize = require("../config/connection");
 
-class Post extends Model {}
+// class Post extends Model {}
 
-Post.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
-            allowNull: false,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        post_content: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        user_id: {
-            type: DataTypes.UUID,
-            references: {
-                model: "user",
-                key: "id",
-            },
-        },
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        modelName: "post",
-    }
-);
+// Post.init(
+//     {
+//         id: {
+//             type: DataTypes.UUID,
+//             defaultValue: DataTypes.UUIDV4,
+//             primaryKey: true,
+//             allowNull: false,
+//         },
+//         title: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         post_content: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         user_id: {
+//             type: DataTypes.UUID,
+//             references: {
+//                 model: "user",
+//                 key: "id",
+//             },
+//         },
+//     },
+//     {
+//         sequelize,
+//         freezeTableName: true,
+//         modelName: "post",
+//     }
+// );
 
-module.exports = Post;
+// module.exports = Post;
 
 // Path: models\post.js
 
