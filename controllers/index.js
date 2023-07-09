6839a82315843547eed896e66ce60b20967aa7d5
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-08 21:24:27 -0700

    started authentication

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const apiRoutes = require("./apiRoutes");
+const authController = require("./authController")
 
 router.use("/api", apiRoutes);
+router.use("/auth", authController);
 
 module.exports = router;
 
