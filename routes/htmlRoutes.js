9d8a4fc2231377ef562185c77d5fd2a949fe0d84
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-08 21:04:20 -0700

    added routes in htmlRoutes and login.hbs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,6 +5,23 @@ router.get("/", (req, res) => {
   res.render("home");
 });
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/post", (req, res) => {
+  res.render("post");
+});
+
+
 module.exports = router;
 
 // Path: routes\htmlRoutes.js
