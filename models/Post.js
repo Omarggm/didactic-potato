5a72f2518ac7d5119dbf1e812ce4363f9c9e9dcd
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-08 19:14:26 -0700

    updated models

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        post_content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.UUID,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        modelName: "post",
+    }
+);
+
+module.exports = Post;
+
+
---
+++ models/index.js
@@ -0,0 +1,14 @@
+const Post = require("./post");
+const User = require("./user");
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+module.exports = { Post, User };
+
