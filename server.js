cb865fa0c830f11178af92b22665de1ac3a20b8a
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-27 07:46:19 -0700

    reset up server.js

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 const express = require("express");
+const exphs = require("express-handlebars");
 const session = require("express-session");
-const exphbs = require("express-handlebars");
-const routes = require("./routes");
 const controllers = require("./controllers");
-const dotenv = require("dotenv");
-const sequelize = require("./config/connection");
-const hbs = exphbs.create({});
+const routes = require("./routes");
+const path = require("path");
 
-//Load the environment variables from the .env file
-dotenv.config();
+const hbs = exphs.create({});
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-
-//Set up sessions middleware
-app.use(
-  session({
-    secret: "Super secret secret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-//Set up handlebars as the template engine
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-//Set up routes
+const sess = {
+  secret: "Super secret secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 2,
+  },
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(express.static(__dirname + `/public`));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+
 app.use(routes);
 app.use(controllers);
 
-//Sync sequelize models to the database, then turn on the server
+// Synchronize the Sequelize models with the database
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now live on http://localhost/${PORT}`));
+  // Start the Express server and listen for incoming requests on the specified port
+  app.listen(PORT, () => console.log(`Now live on http://localhost:${PORT}`));
 });
-
-// Path: server.js
