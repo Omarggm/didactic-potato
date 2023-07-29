fe50db6f9b7d714f8abdd8d6fe7be49084f04b64
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-07-28 22:55:47 -0700

    added cssstyling to page

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,25 +1,14 @@
-const signupFormHandler = async (event) => {
-  event.preventDefault();
+const logout = async () => {
+  const response = await fetch("/api/author/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (name && email && password) {
-    const response = await fetch("/api/author", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
   }
 };
 
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+document.querySelector("#logout").addEventListener("click", logout);
